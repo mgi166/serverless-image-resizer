b09ab9ce1a6cf36522d901fb9ae7d436941262ed
mgi166 <skskoari@gmail.com>
2016-11-06 16:03:24 +0900

    Download s3 image file.

--- index.js
+++ index.js
@@ -2,8 +2,37 @@
 
 import 'babel-polyfill';
 import gm from 'gm';
+import Aws from 'aws-sdk';
+import path from 'path';
+import fs from 'fs';
+
+const s3 = new Aws.S3();
+
+const downloadImage = (params) => {
+  return new Promise((resolve, reject) => {
+    // const destPath = path.join('/tmp', (new Date()).getTime().toString());
+    const destPath = "/test";
+    s3.getObject(params).promise()
+      .then(data => {
+        fs.writeFileSync(destPath, data.Body);
+        return resolve(destPath);
+      })
+      .catch(err => reject(err));
+    });
+};
 
 module.exports.handle = (event, context, callback) => {
   console.log('eventObject: %s', JSON.stringify(event, null, 2));
+
+  const s3Event = event.Records[0].s3;
+  const params = {
+    Bucket: s3Event.bucket.name,
+    Key: s3Event.object.key,
+  };
+
+  Promise.resolve()
+    .then(() => downloadImage(params))
+    .catch((err) => console.log(err));
+
   callback(null, event);
 };
