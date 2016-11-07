b3f1a7f5c20251dc47bbaa57ec04d37382c4d012
mgi166 <skskoari@gmail.com>
2016-11-07 23:23:01 +0900

    Logs `context` object.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import imageResizer from './lib/imageResizer';
 
 export const handle = (event, context, callback) => {
   console.log('eventObject: %s', JSON.stringify(event, null, 2));
+  console.log('context: %s', JSON.stringify(context, null, 2));
 
   imageResizer(event)
     .then(data => callback(null, data))
