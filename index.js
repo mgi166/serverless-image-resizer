013f37419ab894c0eae4821191ddde4357c9c49f
mgi166 <skskoari@gmail.com>
2016-11-07 23:24:20 +0900

    ImageResizer gives `event.Records[0].s3`
    * To less more dependency imageResizer and AWS Lambda

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export const handle = (event, context, callback) => {
   console.log('eventObject: %s', JSON.stringify(event, null, 2));
   console.log('context: %s', JSON.stringify(context, null, 2));
 
-  imageResizer(event)
+  imageResizer(event.Records[0].s3)
     .then(data => callback(null, data))
     .catch(err => callback(err));
 };
