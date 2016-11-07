47324da20f8fa4b46bd0269837f00c5762a82ed7
mgi166 <skskoari@gmail.com>
2016-11-07 13:56:44 +0900

    `imageResizer` receives only event object.
    * imageResizer need not to know callback or AWS Lambda logics.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import imageResizer from './lib/imageResizer';
 export const handle = (event, context, callback) => {
   console.log('eventObject: %s', JSON.stringify(event, null, 2));
 
-  imageResizer(event, context, callback)
+  imageResizer(event)
+    .then(data => callback(null, data))
     .catch(err => callback(err));
 };
